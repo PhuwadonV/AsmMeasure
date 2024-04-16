c47a17c07f395579406951c778bd602a02671176
Phuwadon Verasertniyom <phuwadonpeak@hotmail.com>
2024-04-16 11:50:28 +0700

    Add Template/MeasureMicroarchitecture project

---
+++ Projects/Template/MeasureMicroarchitecture/src/Main.asm
@@ -0,0 +1,93 @@
+INCLUDELIB msvcrt.lib
+INCLUDELIB legacy_stdio_definitions.lib
+INCLUDE Util/Core.inc
+
+OPTION CASEMAP:NONE
+OPTION PROC:PRIVATE
+
+MEASURE_RETRY_AMOUNT EQU 0FFFh
+
+printf PROTO
+EXTERNDEF __imp_GetCurrentThread     : PTR
+EXTERNDEF __imp_GetCurrentProcess    : PTR
+EXTERNDEF __imp_SetThreadAffinityMask: PTR
+EXTERNDEF __imp_SetPriorityClass     : PTR
+EXTERNDEF __imp_SetThreadPriority    : PTR
+
+.CONST
+format0 DB "|---------------------------|", 0Ah, 0
+format1 DB "|                           |", 0Ah, 0
+format2 DB "| Measure Microarchitecture |", 0Ah, 0
+format3 DB "|                           |", 0Ah, 0
+format4 DB "|---------------------------|", 0Ah, 0
+format5 DB "|                 |   Min   |", 0Ah, 0
+format6 DB "|-----------------|---------|", 0Ah, 0
+format7 DB "|                 | %  7llu |", 0Ah, 0
+
+.CODE
+main PROC PUBLIC
+    Stack_main STRUCT
+        measureMin            QWORD ?
+        measureRetryCountdown DWORD ?
+    Stack_main ENDS
+
+    MaxParam_main = 4
+    @ProcBegin 8 * MaxParam_main + SIZE(Stack_main), rsi, rdi
+
+    @Var Stack_main, stack, [rsp + 8 * MaxParam_main]
+
+    call __imp_GetCurrentThread
+    mov @Param(1, DWORD), 1
+    mov @Param(0, QWORD), rax
+    call __imp_SetThreadAffinityMask
+
+    call __imp_GetCurrentProcess
+    mov @Param(1, DWORD), 100h
+    mov @Param(0, QWORD), rax
+    call __imp_SetPriorityClass
+
+    call __imp_GetCurrentThread
+    mov @Param(1, DWORD), 15
+    mov @Param(0, QWORD), rax
+    call __imp_SetThreadPriority
+
+    @Call printf, OFFSET format0
+    @Call printf, OFFSET format1
+    @Call printf, OFFSET format2
+    @Call printf, OFFSET format3
+    @Call printf, OFFSET format4
+    @Call printf, OFFSET format5
+    @Call printf, OFFSET format6
+
+    mov stack.measureMin, -1
+    mov stack.measureRetryCountdown, MEASURE_RETRY_AMOUNT
+
+    ALIGN 16
+@@:
+    rdtscp
+    lfence
+    mov esi, eax
+    mov edi, edx
+
+    rdtscp
+    lfence
+    shl rdi, 32
+    shl rdx, 32
+    or rdi, rsi
+    or rdx, rax
+    sub rdx, rdi
+
+    cmp rdx, stack.measureMin
+    cmova rdx, stack.measureMin
+    mov stack.measureMin, rdx
+
+    sub stack.measureRetryCountdown, 1
+    jae @B
+
+    @Call printf, OFFSET format7, stack.measureMin
+
+    xor eax, eax
+    @ProcEnd
+main ENDP
+
+END
